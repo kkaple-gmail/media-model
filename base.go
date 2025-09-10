d8bef23857ea889784f4b80d9d7d46daa80582a1
kape <kape@kaple.org>
2025-09-10 16:43:45 -0400

    fix

--- base.go
+++ base.go
@@ -1,31 +1,28 @@
 package model
 
 import (
-  "context"
-  "fmt"
-  "time"
+	"fmt"
+	"time"
 
-  "github.com/google/uuid"
+	"github.com/google/uuid"
 )
 
 type Base struct {
-  ID        uuid.UUID `gorm:"column:id;primary_key" json:"id"`
-  CreatedAt time.Time `json:"-"`
-  UpdatedAt time.Time `json:"-"`
+	ID        uuid.UUID `gorm:"column:id;primary_key" json:"id"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
 }
 
 type Pagination struct {
-  PageNumber int `json:"pageNumber"`
-  PageSize   int `json:"pageSize"`
+	PageNumber int `json:"pageNumber"`
+	PageSize   int `json:"pageSize"`
 }
 
 func MustParse(s string) uuid.UUID {
-  u, err := uuid.Parse(s)
-  if err != nil {
-    fmt.Errorf("error generating uuid from string %s: %w", s, err)
-    return uuid.UUID{}
-  }
-  return u
+	u, err := uuid.Parse(s)
+	if err != nil {
+		fmt.Errorf("error generating uuid from string %s: %w", s, err)
+		return uuid.UUID{}
+	}
+	return u
 }
-
-
