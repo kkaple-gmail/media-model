a999d9268f1bb6956df79c59538479c31019d134
kape <kape@kaple.org>
2025-09-10 18:30:11 -0400

    foo

--- uploaded_file.go
+++ uploaded_file.go
@@ -1,7 +1,14 @@
 package model
 
+import "github.com/google/uuid"
+
 type UploadedFile struct {
 	Base
 	OriginalName string `json:"originalName"`
 	Size         uint64 `json:"size"`
+}
+
+func NewUploadedFile(size uint64) *UploadFile {
+	id = uuid.New()
+	return &UploadFile{ID: id, Size: size}
 }
