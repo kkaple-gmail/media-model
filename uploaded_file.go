f8379026a97e6d424cd0a11055d8323e6fd695eb
kape <kape@kaple.org>
2025-09-17 17:17:26 -0400

    updatestruct

--- uploaded_file.go
+++ uploaded_file.go
@@ -2,16 +2,36 @@ package model
 
 import "github.com/google/uuid"
 
+type UploadedFileState string
+
+const (
+	New        UploadedFileState = "new"
+	Duplicate  UploadedFileState = "duplicate"
+	Processing UploadedFileState = "processing"
+	Processed  UploadedFileState = "processed"
+	Error      UploadedFileState = "error"
+)
+
 type UploadedFile struct {
 	Base
-	OriginalName string `json:"originalName"`
-	Size         uint64 `json:"size"`
+	OriginalName string            `json:"original_name"`
+	Size         uint64            `json:"size"`
+	MIME         string            `json:"mime"`
+	SHA256       []byte            `json:"sha256"`
+	State        UploadedFileState `json:"state"`
 }
 
-func NewUploadedFile(size uint64) *UploadedFile {
+func NewUploadedFile(
+	origName string,
+	size uint64,
+	mime string,
+	sha256 []byte) *UploadedFile {
 	id := uuid.New()
 	return &UploadedFile{
-		Base: Base{ID: id},
-		Size: size,
+		Base:         Base{ID: id},
+		OriginalName: origName,
+		Size:         size,
+		Mime:         mime,
+		SHA256:       sha256,
 	}
 }
